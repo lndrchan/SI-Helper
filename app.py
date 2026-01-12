f200df0c99635626764cfa80641bead8d6162fd5
lndrchan <untitledaep@gmail.com>
2026-01-12 05:45:48 +0000

    Converted image format

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify, session
+import git
 
 app = Flask(__name__)
 
@@ -8,3 +9,13 @@ def main():
         return
     else:
         return render_template("main.html")
+
+@app.route('/update-server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/user/lndr/files/home/lndr/SI-Helper/.git')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
